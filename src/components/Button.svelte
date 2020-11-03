c85abc2497f9fa8f4a853df41a03a5e2895c5b3e
Johan Myrsmeden <johan.myrsmeden@storytelconsultant.com>
2020-11-03 16:07:23 +0100

    Adding demo component

---
+++ src/components/Button.svelte
@@ -0,0 +1,10 @@
+<style>
+    .button {
+        padding: 10px 20px;
+        background-color: #f50;
+        color: #fff;
+        font-weight: bold;
+    }
+</style>
+
+<button class="button"><slot /></button>
