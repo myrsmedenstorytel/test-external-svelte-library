60f4a23533664e6eb3712b812b316d9bb12e1617
Johan Myrsmeden <johan.myrsmeden@storytelconsultant.com>
2020-11-04 07:50:52 +0100

    Adding Icon

--- src/components/Button.svelte
+++ src/components/Button.svelte
@@ -1,10 +1,15 @@
+<script>
+  import Icon from "./Icon.svelte";
+</script>
+
 <style>
-    .button {
-        padding: 10px 20px;
-        background-color: #f50;
-        color: #fff;
-        font-weight: bold;
-    }
+  .button {
+    padding: 10px 20px;
+    background-color: #f50;
+    color: #fff;
+    font-weight: bold;
+  }
 </style>
 
-<button class="button"><slot /></button>
+<button class="button"><Icon />
+  <slot /></button>
